573c83301eb91d9fca9a1b0bf147837caf7907d5
uimarshall <uimarshall@gmail.com>
2024-01-07 15:08:18 +0000

    Add splash screen
    splash screen is added to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shoprise/screens/home_screen.dart';
+// import 'package:shoprise/screens/home_screen.dart';
+import 'package:shoprise/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
